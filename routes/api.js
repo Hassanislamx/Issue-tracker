9c743f90c07a4d5ab915296bf25c2fc07f8f470f
Hassan Islam <138744113+Hassanislamx@users.noreply.github.com>
2025-09-23 08:18:21 +0000

    commit

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,16 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 const issueSchema = new mongoose.Schema({
-  project: String,
   issue_title: { type: String, required: true },
   issue_text: { type: String, required: true },
   created_by: { type: String, required: true },
   assigned_to: { type: String, default: "" },
   status_text: { type: String, default: "" },
-  created_on: { type: Date, default: new Date() },
-  updated_on: { type: Date, default: new Date() },
+  created_on: { type: Date, default: Date.now },
+  updated_on: { type: Date, default: Date.now },
   open: { type: Boolean, default: true }
 });
+
 
 const Issue = mongoose.model('Issue', issueSchema);
 
